3e46de5f05ed37aa0fb05b1f94cbb17cfa7b94d7
Sravanthi <sravanthiarvapalli@gmail.com>
2017-08-21 09:44:20 +0530

    Added Sample program

---
+++ src/test/java/com/Test1/CompanyName/ProjectName/TestScripts/SampleClass.java
@@ -0,0 +1,10 @@
+package com.Test1.CompanyName.ProjectName.TestScripts;
+
+public class SampleClass {
+	public static void main(String args[])
+	{
+		System.out.println("Hello Everyone");
+	}
+
+
+}
